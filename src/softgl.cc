8a121ff17492c0242b74101141abb91740639425
Syoyo Fujita <syoyo@lighttransport.com>
2017-01-28 17:42:47 +0900

    Implement some OpenGL API.

--- src/softgl.cc
+++ src/softgl.cc
@@ -1,25 +1,292 @@
 #include "softgl.h"
 
-namespace softgl {
+#include <cassert>
+#include <vector>
 
-class SoftGLContext {
- public:
-  SoftGLContext() {}
-  ~SoftGLContext() {}
+namespace softgl
+{
 
+const int kMaxUniforms = 64;
+const int kMaxBuffers = 64;
+
+struct Buffer
+{
+    std::vector<uint8_t> data;
+    bool deleted;
+    char pad[7];
+
+    Buffer()
+    {
+        deleted = true;
+    }
 };
 
-static SoftGLContext* gCtx;
+struct Accessor
+{
+    size_t offset;
+    size_t size;
+    uint32_t buffer_index;
+    bool assigned;
+    char pad[7];
+    int pad1;
+
+    Accessor()
+    {
+        offset = 0;
+        size = 0;
+        buffer_index = 0;
+        assigned = false;
+    }
+};
+
+struct Uniform
+{
+    union {
+        float fv[4];
+        int iv[4];
+    };
+
+    int type;
+    int count;
+};
+
+class SoftGLContext
+{
+public:
+    SoftGLContext()
+    {
+        // 0th index is reserved.
+        uniforms.resize(kMaxUniforms + 1);
+        buffers.resize(kMaxBuffers + 1);
+        shader_storage_buffer_accessor.resize(kMaxBuffers + 1);
+        uniform_buffer_accessor.resize(kMaxBuffers + 1);
+    }
+
+    ~SoftGLContext()
+    {
+    }
+
+    uint32_t active_buffer_index;
+    int pad0;
+
+    std::vector<Accessor> shader_storage_buffer_accessor;
+    std::vector<Accessor> uniform_buffer_accessor;
+    std::vector<Uniform> uniforms;
+    std::vector<Buffer> buffers;
+};
+
+static SoftGLContext *gCtx = nullptr;
 
 void InitSoftGL()
 {
-  gCtx = new SoftGLContext();
+    gCtx = new SoftGLContext();
 }
 
 void ReleaseSoftGL()
 {
-  delete gCtx;
-  gCtx = nullptr;
+    delete gCtx;
+    gCtx = nullptr;
 }
 
-}  // namespace softgl
+static void InitializeGLContext()
+{
+    if (gCtx == nullptr)
+    {
+        InitSoftGL();
+    }
+}
+
+void glUniform1f(GLint location, GLfloat v0)
+{
+    InitializeGLContext();
+    assert(location >= 0);
+    assert(location < kMaxUniforms);
+
+    gCtx->uniforms[static_cast<size_t>(location)].fv[0] = v0;
+    gCtx->uniforms[static_cast<size_t>(location)].type = GL_FLOAT;
+    gCtx->uniforms[static_cast<size_t>(location)].count = 1;
+}
+
+void glUniform2f(GLint location, GLfloat v0, GLfloat v1)
+{
+
+    InitializeGLContext();
+    assert(location >= 0);
+    assert(location < kMaxUniforms);
+
+    gCtx->uniforms[static_cast<size_t>(location)].fv[0] = v0;
+    gCtx->uniforms[static_cast<size_t>(location)].fv[1] = v1;
+    gCtx->uniforms[static_cast<size_t>(location)].type = GL_FLOAT;
+    gCtx->uniforms[static_cast<size_t>(location)].count = 2;
+}
+
+void glUniform3f(GLint location, GLfloat v0, GLfloat v1, GLfloat v2)
+{
+    InitializeGLContext();
+    assert(location >= 0);
+    assert(location < kMaxUniforms);
+
+    gCtx->uniforms[static_cast<size_t>(location)].fv[0] = v0;
+    gCtx->uniforms[static_cast<size_t>(location)].fv[1] = v1;
+    gCtx->uniforms[static_cast<size_t>(location)].fv[2] = v2;
+    gCtx->uniforms[static_cast<size_t>(location)].type = GL_FLOAT;
+    gCtx->uniforms[static_cast<size_t>(location)].count = 3;
+}
+
+void glUniform4f(GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3)
+{
+    InitializeGLContext();
+    assert(location >= 0);
+    assert(location < kMaxUniforms);
+
+    gCtx->uniforms[static_cast<size_t>(location)].fv[0] = v0;
+    gCtx->uniforms[static_cast<size_t>(location)].fv[1] = v1;
+    gCtx->uniforms[static_cast<size_t>(location)].fv[2] = v2;
+    gCtx->uniforms[static_cast<size_t>(location)].fv[3] = v3;
+    gCtx->uniforms[static_cast<size_t>(location)].type = GL_FLOAT;
+    gCtx->uniforms[static_cast<size_t>(location)].count = 4;
+}
+
+void glUniform1i(GLint location, GLint v0)
+{
+    InitializeGLContext();
+    assert(location >= 0);
+    assert(location < kMaxUniforms);
+
+    gCtx->uniforms[static_cast<size_t>(location)].iv[0] = v0;
+    gCtx->uniforms[static_cast<size_t>(location)].type = GL_INT;
+    gCtx->uniforms[static_cast<size_t>(location)].count = 1;
+}
+
+void glUniform2i(GLint location, GLint v0, GLint v1)
+{
+    InitializeGLContext();
+    assert(location >= 0);
+    assert(location < kMaxUniforms);
+
+    gCtx->uniforms[static_cast<size_t>(location)].iv[0] = v0;
+    gCtx->uniforms[static_cast<size_t>(location)].iv[1] = v1;
+    gCtx->uniforms[static_cast<size_t>(location)].type = GL_INT;
+    gCtx->uniforms[static_cast<size_t>(location)].count = 2;
+}
+
+void glUniform3i(GLint location, GLint v0, GLint v1, GLint v2)
+{
+    InitializeGLContext();
+    assert(location >= 0);
+    assert(location < kMaxUniforms);
+
+    gCtx->uniforms[static_cast<size_t>(location)].iv[0] = v0;
+    gCtx->uniforms[static_cast<size_t>(location)].iv[1] = v1;
+    gCtx->uniforms[static_cast<size_t>(location)].iv[2] = v2;
+    gCtx->uniforms[static_cast<size_t>(location)].type = GL_INT;
+    gCtx->uniforms[static_cast<size_t>(location)].count = 3;
+}
+
+void glUniform4i(GLint location, GLint v0, GLint v1, GLint v2, GLint v3)
+{
+    InitializeGLContext();
+    assert(location >= 0);
+    assert(location < kMaxUniforms);
+
+    gCtx->uniforms[static_cast<size_t>(location)].iv[0] = v0;
+    gCtx->uniforms[static_cast<size_t>(location)].iv[1] = v1;
+    gCtx->uniforms[static_cast<size_t>(location)].iv[2] = v2;
+    gCtx->uniforms[static_cast<size_t>(location)].iv[3] = v3;
+    gCtx->uniforms[static_cast<size_t>(location)].type = GL_INT;
+    gCtx->uniforms[static_cast<size_t>(location)].count = 4;
+}
+
+void glGenBuffers(GLsizei n, GLuint *buffers)
+{
+    InitializeGLContext();
+    assert(n >= 0);
+    assert(n < kMaxBuffers);
+    assert(buffers);
+
+    // Simple linear seach to find available Buffer.
+    // Skip 0th index since its reserved.
+    int count = 0;
+    for (size_t i = 1; i < gCtx->buffers.size(); i++)
+    {
+        if (gCtx->buffers[i].deleted)
+        {
+            buffers[count] = static_cast<GLuint>(i);
+            gCtx->buffers[i].deleted = false;
+            count++;
+
+            if (count >= n)
+                break;
+        }
+    }
+}
+
+void glBindBuffer(GLenum target, GLuint buffer)
+{
+    InitializeGLContext();
+    assert((target == GL_SHADER_STORAGE_BUFFER) || (target == GL_UNIFORM_BUFFER));
+    (void)target;
+
+    gCtx->active_buffer_index = buffer;
+}
+
+void glBindBufferBase(GLenum target, GLuint index, GLuint buffer)
+{
+    InitializeGLContext();
+    assert((target == GL_SHADER_STORAGE_BUFFER) || (target == GL_UNIFORM_BUFFER));
+
+    assert(index < kMaxBuffers);
+
+    assert(buffer < kMaxBuffers);
+    assert(gCtx->buffers[buffer].deleted == false);
+
+    if (target == GL_SHADER_STORAGE_BUFFER)
+    {
+    }
+    else if (target == GL_UNIFORM_BUFFER)
+    {
+    }
+}
+
+void glBindBufferRange(GLenum target, GLuint index, GLuint buffer, GLintptr offset, GLsizeiptr size)
+{
+    InitializeGLContext();
+    assert((target == GL_SHADER_STORAGE_BUFFER) || (target == GL_UNIFORM_BUFFER));
+
+    assert(index < kMaxBuffers);
+    assert(buffer < kMaxBuffers);
+    assert(gCtx->buffers[buffer].deleted == false);
+
+    assert(static_cast<size_t>(offset + size) <= gCtx->buffers[buffer].data.size());
+
+    if (target == GL_SHADER_STORAGE_BUFFER)
+    {
+        gCtx->shader_storage_buffer_accessor[index].assigned = true;
+        gCtx->shader_storage_buffer_accessor[index].buffer_index = buffer;
+        gCtx->shader_storage_buffer_accessor[index].offset = static_cast<size_t>(offset);
+        gCtx->shader_storage_buffer_accessor[index].size = static_cast<size_t>(size);
+    }
+    else if (target == GL_UNIFORM_BUFFER)
+    {
+        gCtx->uniform_buffer_accessor[index].assigned = true;
+        gCtx->uniform_buffer_accessor[index].buffer_index = buffer;
+        gCtx->uniform_buffer_accessor[index].offset = static_cast<size_t>(offset);
+        gCtx->uniform_buffer_accessor[index].size = static_cast<size_t>(size);
+    }
+}
+
+void glBufferData(GLenum target, GLsizeiptr size, const GLvoid *data, GLenum usage)
+{
+    InitializeGLContext();
+    assert((target == GL_SHADER_STORAGE_BUFFER) || (target == GL_UNIFORM_BUFFER));
+
+    if (gCtx->active_buffer_index == 0)
+        return;
+
+    gCtx->buffers[gCtx->active_buffer_index].data.resize(static_cast<size_t>(size));
+    memcpy(gCtx->buffers[gCtx->active_buffer_index].data.data(), data, static_cast<size_t>(size));
+
+    (void)usage;
+}
+
+} // namespace softgl
--- src/softgl.h
+++ src/softgl.h
@@ -1,12 +1,13 @@
 #ifndef SOFT_GL_H_
 #define SOFT_GL_H_
 
-#include <cstdint>
 #include <cstddef>
+#include <cstdint>
 
 // Simple software emulation of some OpenGL functions.
 
-namespace softgl {
+namespace softgl
+{
 
 typedef uint8_t GLboolean;
 typedef uint32_t GLenum;
@@ -26,6 +27,14 @@ typedef char GLchar;
 typedef ptrdiff_t GLintptr;
 typedef ptrdiff_t GLsizeiptr;
 
+const int GL_BYTE = 0x1400;
+const int GL_UNSIGNED_BYTE = 0x1401;
+const int GL_SHORT = 0x1402;
+const int GL_UNSIGNED_SHORT = 0x1403;
+const int GL_INT = 0x1404;
+const int GL_UNSIGNED_INT = 0x1405;
+const int GL_FLOAT = 0x1406;
+
 const int GL_SHADER_STORAGE_BARRIER_BIT = 0x2000;
 const int GL_MAX_COMBINED_SHADER_OUTPUT_RESOURCES = 0x8F39;
 const int GL_SHADER_STORAGE_BUFFER = 0x90D2;
@@ -43,66 +52,44 @@ const int GL_MAX_SHADER_STORAGE_BUFFER_BINDINGS = 0x90DD;
 const int GL_MAX_SHADER_STORAGE_BLOCK_SIZE = 0x90DE;
 const int GL_SHADER_STORAGE_BUFFER_OFFSET_ALIGNMENT = 0x90DF;
 
-void glUniform1f( GLint location,
-  GLfloat v0);
- 
-void glUniform2f( GLint location,
-  GLfloat v0,
-  GLfloat v1);
- 
-void glUniform3f( GLint location,
-  GLfloat v0,
-  GLfloat v1,
-  GLfloat v2);
- 
-void glUniform4f( GLint location,
-  GLfloat v0,
-  GLfloat v1,
-  GLfloat v2,
-  GLfloat v3);
- 
-void glUniform1i( GLint location,
-  GLint v0);
- 
-void glUniform2i( GLint location,
-  GLint v0,
-  GLint v1);
- 
-void glUniform3i( GLint location,
-  GLint v0,
-  GLint v1,
-  GLint v2);
- 
-void glUniform4i( GLint location,
-  GLint v0,
-  GLint v1,
-  GLint v2,
-  GLint v3);
+const int GL_UNIFORM_BUFFER = 0x8A11;
 
-GLint glGetUniformLocation( GLuint program,
-  const GLchar *name);
+void glUniform1f(GLint location, GLfloat v0);
 
-void glDispatchCompute( GLuint num_groups_x,
-  GLuint num_groups_y,
-  GLuint num_groups_z);
+void glUniform2f(GLint location, GLfloat v0, GLfloat v1);
 
-void glDispatchComputeIndirect( GLintptr indirect);
+void glUniform3f(GLint location, GLfloat v0, GLfloat v1, GLfloat v2);
 
-void glGenBuffers(  GLsizei n,
-  GLuint * buffers);
+void glUniform4f(GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
+
+void glUniform1i(GLint location, GLint v0);
+
+void glUniform2i(GLint location, GLint v0, GLint v1);
+
+void glUniform3i(GLint location, GLint v0, GLint v1, GLint v2);
+
+void glUniform4i(GLint location, GLint v0, GLint v1, GLint v2, GLint v3);
+
+GLint glGetUniformLocation(GLuint program, const GLchar *name);
+
+void glDispatchCompute(GLuint num_groups_x, GLuint num_groups_y, GLuint num_groups_z);
+
+void glDispatchComputeIndirect(GLintptr indirect);
+
+void glGenBuffers(GLsizei n, GLuint *buffers);
 
 void glBindBuffer(GLenum target, GLuint buffer);
 void glBindBufferBase(GLenum target, GLuint index, GLuint buffer);
 void glBindBufferRange(GLenum target, GLuint index, GLuint buffer, GLintptr offset, GLsizeiptr size);
 
-void * glMapBuffer(GLenum target, GLenum access);
-GLboolean glUnmapBuffer(GLenum target);
+void glBufferData(GLenum target, GLsizeiptr size, const GLvoid *data, GLenum usage);
 
+void *glMapBuffer(GLenum target, GLenum access);
+GLboolean glUnmapBuffer(GLenum target);
 
 void InitSoftGL();
 void ReleaseSoftGL();
 
-
-}  // softgl
+} // softgl
 
 #endif // SOFT_GL_H_
