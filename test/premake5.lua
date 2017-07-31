783004ca1b6e13324e09fc265d57c3ea72a19829
Syoyo Fujita <syoyo@lighttransport.com>
2017-07-31 19:22:46 +0900

    Refactor premake script.

--- test/premake5.lua
+++ test/premake5.lua
@@ -1,19 +1,26 @@
 project "Test_SoftCompute"
 
-if _OPTIONS["ios"] then
-	kind "WindowedApp"
-else	
-	kind "ConsoleApp"
-end
+   if _OPTIONS["ios"] then
+   	kind "WindowedApp"
+   else	
+   	kind "ConsoleApp"
+   end
+   
+   
+   includedirs { "../src", "../third_party/Catch/include"}
+   
+   language "C++"
+   
+   flags { "c++11" }
+   
+   files {
+   	"test-*.cc",
+   	"test-*.h",
+   }
+   
+   links { "SoftCompute" }
 
+   configuration { "linux" }
+      links { "dl", "pthread" }
 
-includedirs { "../src", "../third_party/Catch/include"}
-
-language "C++"
-
-files {
-	"test-*.cc",
-	"test-*.h",
-}
-
-links { "SoftCompute" }
+   configuration { "macosx" }
